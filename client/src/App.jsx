8ff87568c8dff6d15fe87d1b2dae4d0caf97aa13
sans-creator <sanskarj163@gmail.com>
2025-07-23 13:18:41 +0530

    frontendDone

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Route,Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import BuyCredit from './pages/BuyCredit'
+import Result from './pages/Result'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Login from './components/Login'
+import { AppContext } from './context/AppContext'
 
 const App = () => {
+
+  const {showLogin}=useContext(AppContext)
   return (
-    <div>
+    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50'>
+
+    <Navbar/>
+{  showLogin &&  <Login/>
+}
+      <Routes >
+        <Route path="/" element={<Home/>}/>
+        <Route path="/result" element={<Result/>}/>
+        <Route path="/buy" element={<BuyCredit/>}/>
+      </Routes>
+
+
+    <Footer/>
+
+      
       
     </div>
   )
