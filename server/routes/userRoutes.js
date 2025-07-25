a2ccf4ed17188b15ace00d63d444b4aca8433c33
sans-creator <sanskarj163@gmail.com>
2025-07-25 15:04:36 +0530

    credits&transactionRouter

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,16 @@
 import express from 'express'
-import {registerUser, loginUser} from '../controller/userController.js'
+import {registerUser, loginUser, userCredits, paymentRazorpay} from '../controller/userController.js'
+import userAuth from '../middlewares/auth.js'
 // import userAuth from '../middlewares/auth.js'
 
 const userRouter = express.Router()
 
 userRouter.post('/register', registerUser)
 userRouter.post('/login', loginUser)
+userRouter.get('/credits' , userAuth // calling middleware
+, userCredits)
+userRouter.post('/pay-razor' , userAuth ,paymentRazorpay)
+
 // userRouter.get('/credits', userAuth, userCredits)
 export default userRouter
 
